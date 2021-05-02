ce541fb488f947166abaa2f6ccdab897ea132ecb
Andrey Nikonov <anni@ciklum.com>
2021-05-02 11:02:43 +0300

    Improve tests

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as YAML from 'yamljs';
 import { AppModule } from './app.module';
 
@@ -10,8 +11,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const openapiSpecPath = path.resolve(__dirname, 'openapi.yaml');
-  const document = YAML.parseFile(openapiSpecPath);
-  SwaggerModule.setup('api', app, document, { customSiteTitle: 'Github User Repositories' });
+
+  if (fs.existsSync(openapiSpecPath)) {
+    const document = YAML.parseFile(openapiSpecPath);
+    SwaggerModule.setup('api', app, document, { customSiteTitle: 'Github User Repositories' });
+  }
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
--- test/github/get-user-repos.e2e-spec.ts
+++ test/github/get-user-repos.e2e-spec.ts
@@ -2,8 +2,12 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { HttpService, INestApplication, ValidationPipe } from '@nestjs/common';
 import { of, throwError } from 'rxjs';
-import { AxiosError } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
 import { GithubModule } from '../../src/github/infrastructure/github.module';
+import { GitUser } from '../../src/github/infrastructure/type/git-user';
+import { UserTypeEnum } from '../../src/github/infrastructure/type/user-type.enum';
+import { GitRepository } from '../../src/github/infrastructure/type/git-repository';
+import { GitBranch } from '../../src/github/infrastructure/type/git-branch';
 
 describe('Get github user repositories', () => {
   let app: INestApplication;
@@ -22,14 +26,16 @@ describe('Get github user repositories', () => {
   });
 
   it('check 406 Not Acceptable response', () => {
+    const response = {
+      status: 406,
+      message: 'Not Acceptable',
+    };
+
     return request(app.getHttpServer())
       .get('/api/v1/users/username/repos')
       .set('Accept', 'application/xml')
       .expect(406)
-      .expect({
-        status: 406,
-        message: 'Not Acceptable',
-      });
+      .expect(response);
   });
 
   it('check 404 Not Found response', () => {
@@ -45,133 +51,151 @@ describe('Get github user repositories', () => {
         config: {},
       },
     };
-    httpService.get = jest.fn().mockImplementationOnce(() => throwError(err));
+    const response = {
+      status: 404,
+      message: 'User Not Found',
+    };
+
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() => throwError(err));
 
     return request(app.getHttpServer())
       .get('/api/v1/users/usernotfound/repos')
       .set('Accept', 'application/json')
       .expect(404)
-      .expect({
-        status: 404,
-        message: 'User Not Found',
-      });
+      .expect(response);
   });
 
   it('check success response for user', () => {
-    httpService.get = jest
-      .fn()
-      .mockImplementationOnce(() =>
-        of({
-          data: {
+    const defaultResponseFields = {
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+    };
+    const userGitHubResponse = {
+      data: {
+        login: 'john',
+        type: UserTypeEnum.User,
+      },
+      ...defaultResponseFields,
+    };
+    const repositoryGitHubResponse = {
+      data: [
+        {
+          name: 'nest-js-test',
+          fork: false,
+          owner: {
             login: 'john',
-            type: 'User',
           },
-        }),
-      )
-      .mockImplementationOnce(() =>
-        of({
-          data: [
-            {
-              name: 'nest-js-test',
-              fork: false,
-              owner: {
-                login: 'john',
-              },
-            },
-            {
-              name: 'nest-core',
-              fork: true,
-              owner: {
-                login: 'john',
-              },
-            },
-          ],
-        }),
-      )
-      .mockImplementationOnce(() =>
-        of({
-          data: [
-            {
-              name: 'master',
-              commit: {
-                sha: '8ed4230c8295bba838c2eb7fe6310166d33d3020',
-              },
-            },
-          ],
-        }),
-      );
+        },
+        {
+          name: 'nest-core',
+          fork: true,
+          owner: {
+            login: 'john',
+          },
+        },
+      ],
+      ...defaultResponseFields,
+    };
+    const branchGitHubResponse = {
+      data: [
+        {
+          name: 'master',
+          commit: {
+            sha: '8ed4230c8295bba838c2eb7fe6310166d33d3020',
+          },
+        },
+      ],
+      ...defaultResponseFields,
+    };
+    const response = [
+      {
+        name: 'nest-js-test',
+        ownerLogin: 'john',
+        branches: [
+          {
+            name: 'master',
+            sha: '8ed4230c8295bba838c2eb7fe6310166d33d3020',
+          },
+        ],
+      },
+    ];
+
+    jest
+      .spyOn(httpService, 'get')
+      .mockImplementationOnce(() => of<AxiosResponse<GitUser>>(userGitHubResponse))
+      .mockImplementationOnce(() => of<AxiosResponse<GitRepository[]>>(repositoryGitHubResponse))
+      .mockImplementationOnce(() => of<AxiosResponse<GitBranch[]>>(branchGitHubResponse));
 
     return request(app.getHttpServer())
       .get('/api/v1/users/john/repos')
       .set('Accept', 'application/json')
       .expect(200)
-      .expect([
-        {
-          name: 'nest-js-test',
-          ownerLogin: 'john',
-          branches: [
-            {
-              name: 'master',
-              sha: '8ed4230c8295bba838c2eb7fe6310166d33d3020',
-            },
-          ],
-        },
-      ]);
+      .expect(response);
   });
 
   it('check success response for organization', () => {
-    httpService.get = jest
-      .fn()
-      .mockImplementationOnce(() =>
-        of({
-          data: {
+    const defaultResponseFields = {
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+    };
+    const organizationGitHubResponse = {
+      data: {
+        login: 'nestjs',
+        type: UserTypeEnum.Organization,
+      },
+      ...defaultResponseFields,
+    };
+    const repositoryGitHubResponse = {
+      data: [
+        {
+          name: 'nest-cli',
+          fork: false,
+          owner: {
             login: 'nestjs',
-            type: 'Organization',
           },
-        }),
-      )
-      .mockImplementationOnce(() =>
-        of({
-          data: [
-            {
-              name: 'nest-cli',
-              fork: false,
-              owner: {
-                login: 'nestjs',
-              },
-            },
-          ],
-        }),
-      )
-      .mockImplementationOnce(() =>
-        of({
-          data: [
-            {
-              name: 'master',
-              commit: {
-                sha: '31369b65fc87c0cb30cc2032bb5c2f73fe675997',
-              },
-            },
-          ],
-        }),
-      );
+        },
+      ],
+      ...defaultResponseFields,
+    };
+    const branchGitHubResponse = {
+      data: [
+        {
+          name: 'master',
+          commit: {
+            sha: '31369b65fc87c0cb30cc2032bb5c2f73fe675997',
+          },
+        },
+      ],
+      ...defaultResponseFields,
+    };
+    const response = [
+      {
+        name: 'nest-cli',
+        ownerLogin: 'nestjs',
+        branches: [
+          {
+            name: 'master',
+            sha: '31369b65fc87c0cb30cc2032bb5c2f73fe675997',
+          },
+        ],
+      },
+    ];
+
+    jest
+      .spyOn(httpService, 'get')
+      .mockImplementationOnce(() => of<AxiosResponse<GitUser>>(organizationGitHubResponse))
+      .mockImplementationOnce(() => of<AxiosResponse<GitRepository[]>>(repositoryGitHubResponse))
+      .mockImplementationOnce(() => of<AxiosResponse<GitBranch[]>>(branchGitHubResponse));
 
     return request(app.getHttpServer())
       .get('/api/v1/users/nestjs/repos')
       .set('Accept', 'application/json')
       .expect(200)
-      .expect([
-        {
-          name: 'nest-cli',
-          ownerLogin: 'nestjs',
-          branches: [
-            {
-              name: 'master',
-              sha: '31369b65fc87c0cb30cc2032bb5c2f73fe675997',
-            },
-          ],
-        },
-      ]);
+      .expect(response);
   });
 
   afterAll(() => {
